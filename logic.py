1f1c8a404578f73eb8e2774425429b295556ff8b
rahulx81x <rahulgouri072@gmailcom>
2021-06-27 20:43:50 +0530

    nothing

--- logic.py
+++ logic.py
@@ -1,7 +1,8 @@
 def winner(player, comp):
     win_count = 0
     for i in range(len(comp)):
-        if (player == 'r' and comp[i][0] == 's') or (player == 's' and comp[i][0] == 'p') or (player == 'p' and comp[i][0] == 'r'):
+        if (player == 'r' and comp[i][0] == 's') or (player == 's' and comp[i][0] == 'p') or\
+                (player == 'p' and comp[i][0] == 'r'):
             win_count += 1
     print(comp)
     return win_count
